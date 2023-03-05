09ea9d4829be4952f65bda4093a4f71b825d2a28
Kononov <akellastoopi@gmail.com>
2023-03-05 12:59:19 +0200

    landing page, add scss stylesheets

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./login.scss";
+const Login: React.FC = (): JSX.Element => {
+	return <div className="login-container">Login here</div>;
+};
+
+export default Login;
